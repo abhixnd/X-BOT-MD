b5b1d8cd59da1330c3e2050e82c988d709b3f618
abhixnd <166606042+abhixnd@users.noreply.github.com>
2024-08-18 14:45:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ if (fs.existsSync('config.env')) {
 
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
-const HANDLERS = process.env.HANDLER || "^";
+const HANDLERS = process.env.HANDLER || ".";
 const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const SUDO = process.env.SUDO || '919544949116';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
